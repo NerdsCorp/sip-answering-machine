7ee6c7c0ead070cdd3dc0864280247cee45d1407
NerdsCorpx <86178816+NerdsCorp@users.noreply.github.com>
2025-06-12 14:48:40 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app import baresip_monitor, web_gui
 
-if __name__ == "__main__":Add commentMore actions
+if __name__ == "__main__":
     baresip_monitor.start_baresip_monitor()
     web_gui.start_web_gui()
