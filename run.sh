875fc91addc12fa10a91b55a5bf72973175471a5
NerdsCorpx <86178816+NerdsCorp@users.noreply.github.com>
2025-06-12 14:40:19 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Start baresip_monitor using module syntax
-echo "Starting baresip_monitor..."
-python3 -m app.main &
-
-# Start the web_gui using Gunicorn
-echo "Starting web_gui with Gunicorn..."
-gunicorn app.web_gui:app --bind 0.0.0.0:8000 &
-
-echo "Both services have been started."
-
-wait -n
+python3 -m app.main
